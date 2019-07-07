373c286a2131eea5433b8290771cd8ffd21eedc4
imvr-24 <veerareddy596@gmail.com>
2019-07-07 17:12:39 +0530

    Custom HTTP Observable

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,15 +1,90 @@
-import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation, OnDestroy } from '@angular/core';
+import { interval, timer, fromEvent, Observable, noop } from 'rxjs';
+import { response } from 'express';
 
 @Component({
   selector: 'about',
   templateUrl: './about.component.html',
   styleUrls: ['./about.component.css']
 })
-export class AboutComponent implements OnInit {
+export class AboutComponent implements OnInit, OnDestroy {
 
   constructor() { }
 
   ngOnInit() {
+
+    // promise vs observable
+
+    // promise -- will be immediately executed.
+    // observable -- I will only trigger the request once any subsciption happens.
+
+    // 1. create custom http Observable.
+
+    // defination of http stream.
+    const http$ = Observable.create(observer => {
+      fetch('/api/courses')
+            .then(res => {
+              return res.json();
+            })
+            .then(body => {
+              observer.next(body);
+
+              observer.complete();
+
+            })
+            .catch(err => {
+              observer.error(err);
+            });
+    });
+
+    // creation of http stream.
+    http$.subscribe(
+      courses => console.log(courses),
+      noop,
+      // (err) => {},
+      () => { console.log('Completed..') }
+    );
+
+    // rxjs - reactive extensions for javascript.
+
+    // stream of values.
+
+    // What is an rxjs observable.
+
+    // const interval$ = interval(1000); // defination of stream of values.
+    // const interval$ = timer(1000, 1000); // defination of stream of values.
+
+    // interval$.subscribe(val => console.log('stream 1 => ' + val)); // Creation of stream of values.
+    // const sub = interval$.subscribe(val => console.log('stream 2 => ' + val)); // Creaton of stream of values.
+
+    // setTimeout(() => {
+    //   sub.unsubscribe();
+    // }, 5000);
+
+    // const click$ = fromEvent(document, 'click'); // defination of stream.
+    // click$.subscribe(
+      // event => console.log(event),
+      // err => console.log(err),
+      // () => console.log('Stream is completed!!')
+    // ); // stream of values.
+
+    // complex way of combining streams.
+
+    // document.addEventListener('click', clickevnt => {
+    //   console.log(clickevnt);
+    //   setTimeout(() => {
+    //     console.log(`Timeput has elapsed`);
+    //     let counter = 0;
+    //     setInterval(() => {
+    //       console.log(counter);
+    //       counter++;
+    //     }, 1000);
+    //   }, 3000);
+    // });
+
   }
 
+  ngOnDestroy() {
+
+  }
 }
--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,14 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import {HomeComponent} from "./home/home.component";
-import {AboutComponent} from "./about/about.component";
-import {CourseComponent} from "./course/course.component";
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { CourseComponent } from './course/course.component';
 
 const routes: Routes = [
     {
-        path: "",
+        path: '',
         component: HomeComponent
-
     },
     {
         path: "about",
@@ -25,7 +24,7 @@ const routes: Routes = [
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
 })
 export class AppRoutingModule { }
