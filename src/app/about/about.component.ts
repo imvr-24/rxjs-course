d79ba5fc7aad1d4ded5410d167d4487947947ce1
imvr-24 <veerareddy596@gmail.com>
2019-07-23 17:52:38 +0530

    different types of subjects

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,7 +1,7 @@
-import {Component, OnInit, ViewEncapsulation} from '@angular/core';
-import {concat, fromEvent, interval, noop, observable, Observable, of, timer, merge} from 'rxjs';
-import {delayWhen, filter, map, take, timeout} from 'rxjs/operators';
-import {createHttpObservable} from '../common/util';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { concat, fromEvent, interval, noop, observable, Observable, of, timer, merge, Subject, BehaviorSubject, AsyncSubject, ReplaySubject } from 'rxjs';
+import { delayWhen, filter, map, take, timeout } from 'rxjs/operators';
+import { createHttpObservable } from '../common/util';
 
 
 @Component({
@@ -13,6 +13,38 @@ export class AboutComponent implements OnInit {
 
     ngOnInit() {
 
+        // Subject observable & an observer
+
+        // BehaviorSubject remembers the last emitted value.
+
+        // AsyncSubject.    async operations like final value of calculations
+        // AsyncSubject value gets the last value emitted before completion
+
+
+        // ReplaySubject. Late subscription will get all the values from early subscription.
+
+
+        // const subject = new Subject();
+        // const subject = new BehaviorSubject(0);
+
+        const subject = new ReplaySubject();
+
+        const series$ = subject.asObservable();
+
+        series$.subscribe(data => console.log('early subscription ' + data));
+
+        subject.next(1);
+        subject.next(2);
+        subject.next(4);
+        subject.next(12);
+
+
+        // subject.complete();
+
+        setTimeout(() => {
+            series$.subscribe(data => console.log('Late subscription ' + data));
+            subject.next(52);
+        }, 3000);
 
     }
 
